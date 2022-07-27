73b515be7d4560edc61d71ab4b192474b7c188b1
Alejandro Martinez <goatmix1@gmail.com>
2022-07-27 01:16:46 -0400

    retrying

--- index.js
+++ index.js
@@ -31,8 +31,9 @@ app.get('/products', (req, res) => {
 });
 
     //set static folder
+    if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
-
+    }  
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
